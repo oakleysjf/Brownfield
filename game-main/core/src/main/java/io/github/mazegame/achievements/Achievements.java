11faa497260c5eb04b2e5b4b76e67b2a35107053
Rubyjay <tdv513@york.ac.uk>
2025-11-30 18:27:27 +0000

    Update Achievements.java
    Changes made: java.util.HashMap and java.util.Map have been imported.
    Overall goal: Achievement feature
    Related deliverable: Product brief

--- game-main/core/src/main/java/io/github/mazegame/achievements/Achievements.java
+++ game-main/core/src/main/java/io/github/mazegame/achievements/Achievements.java
@@ -2,11 +2,14 @@
 add imports and package
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class Achievements {
     public static final Map<AchievementID, Achievement> achievements = new HashMap<>();
     static {
         achievements.put(AcheivementID,PICK_UP_ITEM, new PickUpItemAchievment());
-        achievements.put(AchievementID.GIVE_RED_BOX, new GotRedBoxAchievement());
+        achievements.put(AchievementID.GIVE_RED_BOX, new GiveRedBoxAchievement());
     }
 
     public static Achievement get(AchievementID id){
@@ -15,7 +18,7 @@ public final class Achievements {
 }
 
 class PickUpItemAchievment extends Achievement {
-    public PickUpItemAchievment() {
+    public PickUpItemAchievement() {
         this.id = AchievementID.PICK_UP_ITEM;
         this.title = "Item picked up";
         this.description = "You picked up an item.";
@@ -23,7 +26,7 @@ class PickUpItemAchievment extends Achievement {
 }
 
 class GiveRedBoxAchievement extends Achievement {
-    public GotRedBoxAchievment() {
+    public GiveRedBoxAchievment() {
         this.id = AchievmentID.GIVE_RED_BOX;
         this.title = "Red Box given!";
         this.description = "You have given the red box.";
