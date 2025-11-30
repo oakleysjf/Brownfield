859e67af1ef202d8242e45d2aa0aaf42e1d2a3fc
Rubyjay <tdv513@york.ac.uk>
2025-11-30 18:13:55 +0000

    Update Achievement.java
    Change made: Code moved to a different file
    Overall goal: Achievement feature
    Related deliverable: Product brief

--- game-main/core/src/main/java/io/github/mazegame/achievements/Achievement.java
+++ game-main/core/src/main/java/io/github/mazegame/achievements/Achievement.java
@@ -1,1 +1,31 @@
+/*
+add imports and package
+ */
+
+
+public abstract class Achievement {
+    public AchievementID id;
+    public String title;
+    public String description;
+
+    public boolean unlocked = false;
+
+    public AchievementID getId() { return id; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public boolean isUnlocked() { return unlocked; }
+
+    public void unlock() {
+        if (!unlocked) {
+                unlocked = true;
+                onUnlock();
+        }
+    }
+
+    protected void onUnlock() {
+        /*
+        sound and popup
+        */
+    }
+}
 
