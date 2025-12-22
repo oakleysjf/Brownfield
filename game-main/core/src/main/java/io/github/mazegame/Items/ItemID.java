c4d57ff898d261f1bcdd66a0252478fdcefbb4ea
Rubyjay <tdv513@york.ac.uk>
2025-12-22 16:58:38 +0000

    Add each code's item to the itemID
    Changes made: Add each new item to the itemID file
    Overall goal: Check in code
    Related deliverable: Product brief

--- game-main/core/src/main/java/io/github/mazegame/Items/ItemID.java
+++ game-main/core/src/main/java/io/github/mazegame/Items/ItemID.java
@@ -3,5 +3,10 @@ package io.github.mazegame.items;
 /** Holds the ID for each item for easier access. */
 public enum ItemID {
     RED_TEST_BOX,
-    TEST_BOX
+    TEST_BOX,
+    CODE1_COLLECTABLE,
+    CODE2_COLLECTABLE,
+    CODE3_COLLECTABLE,
+    CODE4_COLLECTABLE,
+    CODE5_COLLECTABLE,
 }
