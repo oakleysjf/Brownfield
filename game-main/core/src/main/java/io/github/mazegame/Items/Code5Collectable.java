d326e0e2bd04701f4350c1822a71e9182beda2e7
Rubyjay <tdv513@york.ac.uk>
2025-12-22 16:57:22 +0000

    Add collectable item for code 5
    Changes made: Add collectable item for code 1
    Overall goal: Check in code
    Related deliverable: Product brief

---
+++ game-main/core/src/main/java/io/github/mazegame/Items/Code5Collectable.java
@@ -0,0 +1,16 @@
+package io.github.mazegame.Items;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import io.github.mazegame.Code;
+import io.github.mazegame.CodeManager;
+
+public class Code5Collectable extends Item{
+
+    public Code5Collectable(Code code){
+        name = ItemID.CODE1_COLLECTABLE;
+        isUsable = true;
+        effect = CodeManager.collectCode(code);
+        image = new Texture(/*texture*/);
+    }
+}
